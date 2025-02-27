a6b7ae394d0f31481b445ed43274471781a38e30
NACXIIX <nicocordano@hotmail.com>
2025-02-27 19:25:58 -0300

    Versión 0.1: Proyecto estructurado con dependencias y script de arranque del proyecto

---
+++ start_linux.sh
@@ -0,0 +1,11 @@
+# creando el env
+python3 -m venv .venv
+
+# activando el entorno
+source .venv/bin/activate
+
+# instalando dependencias
+pip install -r requirements.txt
+
+# corriendo el proyecto
+python3 app.py
